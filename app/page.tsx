330fd7dc1e4566002cbf2bc83ffb2203abb76a00
JamesInopia <jamesangelo5106@gmail.com>
2026-09-07 14:25:03 +0800

    refactor: changed placeholder image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
+import image from "./placeholder.jpg";
 
 export default function Home() {
   const currentYear = new Date().getFullYear();
@@ -28,13 +30,15 @@ export default function Home() {
       </header>
       <main className="flex-1 flex flex-col gap-4 p-6 min-w-0">
         <section className="flex gap-4 p-8 min-w-0">
-          <div className="flex-1 p-2 min-w-0">
-            <img src="/your-image.jpg" alt="Description" className="w-full h-full object-cover"/>
-            <div className="text-6xl">Hello World!</div>
-            <div className="text-4xl">I am,</div>
-            <div className="text-4xl">James</div>
-            <div className="text-4xl">Angelo</div>
-            <div className="text-4xl">Inopia</div>
+          <div className="flex-1 p-2 min-w-0 relative h-96">
+            <Image src={image} alt="hi" fill className="object-cover"/>
+            <div className="absolute inset-0 flex flex-col justify-center text-white">
+              <div className="text-6xl">Hello World!</div>
+              <div className="text-4xl">I am,</div>
+              <div className="text-4xl">James</div>
+              <div className="text-4xl">Angelo</div>
+              <div className="text-4xl">Inopia</div>
+            </div>
           </div>
           <div className="flex-1 p-2 min-w-0">
             <div className="border-b-2 border-white">About Me</div>
