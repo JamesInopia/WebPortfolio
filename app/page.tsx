afd4903398d4e9d36995c4e8657e50d8f428c39a
JamesInopia <jamesangelo5106@gmail.com>
2026-09-07 14:08:42 +0800

    feat: initial content and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,46 @@
-import Image from "next/image";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      <div className="flex min-h-screen">
+      <header className="flex flex-col gap-2 p-6 basis-[20%] h-screen sticky top-0 self-start">
+        <div className="flex flex-col justify-between items-center w-full h-full">
+          <div className="flex flex-col p-4 gap-4">
+            <button>Home</button>
+            <button>Projects</button>
+            <button>Contact</button>
+          </div>
+          <div className="flex flex-col">
+            <div>
+              <Link href="https://github.com/JamesInopia" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:opacity-70 transition-opacity">
+                <FaGithub size={20} />
+                <span>GitHub</span>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col text-sm">
+            &copy; {currentYear} James Angelo Inopia
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+      </header>
+      <main className="flex-1 flex flex-col gap-4 p-6 min-w-0">
+        <section className="flex gap-4 p-8 min-w-0">
+          <div className="flex-1 p-2 min-w-0">
+            <img src="/your-image.jpg" alt="Description" className="w-full h-full object-cover"/>
+            <div className="text-6xl">Hello World!</div>
+            <div className="text-4xl">I am,</div>
+            <div className="text-4xl">James</div>
+            <div className="text-4xl">Angelo</div>
+            <div className="text-4xl">Inopia</div>
+          </div>
+          <div className="flex-1 p-2 min-w-0">
+            <div className="border-b-2 border-white">About Me</div>
+            <div>I am a Software Engineer.. IDK WHAT ELSE TO SAY HUHU</div>
+          </div>
+        </section>
       </main>
     </div>
   );
